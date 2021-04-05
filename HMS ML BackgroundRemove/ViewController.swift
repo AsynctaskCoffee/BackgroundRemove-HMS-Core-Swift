d615d94b589371f0f48f5936d238d51728b80ead
AsynctaskCoffee <asynctaskcoffee@gmail.com>
2021-04-05 13:23:48 +0300

    Initial Commit

---
+++ HMS ML BackgroundRemove/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HMS ML BackgroundRemove
+//
+//  Created by Egemen Dev on 5.04.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
